725a8f19e7039f2ebfe40f8e83e622334c20b5c7
jkotan <jan.kotanski@desy.de>
2025-05-08 12:11:57 +0000

    Build: (0c26f79) Build: (296675a) Build: (31812bb) Build: (039192e) Build: (1751625) Build: (cede259) Build: (96e9ed7) Build: (a21b73c) Build: (ebb6d13) Build: (15e04ac) Build: (373925e) Build: (bd4f001) Build: (8d73d25) Build: (7c88e19) Build: (9a34fc9) Build: (b1d54a8) Build: (416ac84) Build: (08f5216) Build: (e36a9e1) Build: (f9c98ef) Build: (257f6ea) Build: (4c051e4) Build: (69d017d) Build: (3e6bafe) Build: (f5a5455) Build: (6c3555c) Build: (7ba3570) Build: (96c26f8) Build: (6da2ba7) Build: (b5f503e) Build: (b57f9b9) Build: (1de1b35) Build: (4cdf63d) Build: (7e8bdd9) Build: (9790ebc) Build: (40be58e) Build: (8693115) Build: (88f6aac) Build: (aa0e8ac) Build: (4f54ba4) Build: (e54b56d) Build: (3194c13) Build: (584bb12) Build: (34d56e0) Build: (b4c7f73) Build: (1b72728) Build: (01639eb) Build: (c62d0d0) Build: (4a136cd) Build: (5c0e2bd) Build: (8e2b8a1) Build: (1b699a7) Build: (d8480ad) Build: (e7dc4e6) Build: (9119b36) Build: (cc427d3) Build: (583dc7b) Build: (73a0849) Build: (e0bd446) Build: (076f318) Build: (b14c29f) Build: (01f22d3) Build: (376cc3b) Build: (6a4ee16) Build: (34b32c7) Build: (8a7b21f) Build: (48d41ab) Build: (a4375c6) Build: (07ccbb1) Build: (acf4e00) Build: (767b597) Build: (77b390f) Build: (d3a7082) Build: (e1880fe) Build: (940afd4) Build: (61d3a07) Build: (4be3393) Build: (7b1ed53) Build: (9c38ab1) Build: (b889152) Build: (8a98b6b) Build: (58a7106) Build: (1e55616) Build: (49b507a) Build: (7451b1c) Build: (f46fb57) Build: (74e47b2) Build: (7ce565a) Build: (510a022) Build: (dfa014c) Build: (34e02dc) Build: (0405ce7) Build: (4762f4c) Build: (3e47d8d) Build: (414c4d3) Build: (e6ec68c) Build: (96b0525) Build: (db52259) Build: (56ef839) Build: (b169f96) Build: (1225471) Build: (6cde7aa) Build: (55351c5) Build: (4233ac6) Build: (045066c) Build: (eb647b6) Build: (e2c5480) Build: (6f57bb0) Build: (97fc12e) Build: (0f5e8d6) Build: (bc1003a) Build: (249e621) Build: (07f7894) Build: (ee17d0d) Build: (c953785) Build: (4eb2447) remove doc/api

--- stable/_static/bizstyle.js
+++ stable/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "libpninexus 3.2.0 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "libpninexus 3.3.0 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
