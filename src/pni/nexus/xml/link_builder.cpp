4f067e0b08ba03cf724f5d8d5777f47ee4ffe84f
Jan Kotanski <jankotan@gmail.com>
2026-08-06 08:56:35 +0200

    skip links without target

--- src/pni/nexus/xml/link_builder.cpp
+++ src/pni/nexus/xml/link_builder.cpp
@@ -35,6 +35,9 @@ LinkBuilder::LinkBuilder(const Node &node):
 
 void LinkBuilder::build(const hdf5::node::Node &parent) const
 {
+  if (!(node.attributes.exists("target")))
+    return;
+
   hdf5::node::Group link_parent(parent);
   std::string link_name = node().attribute("name").str_data();
   pni::nexus::Path link_target(pni::nexus::Path::from_string(node().attribute("target").str_data()));
