f8aa19a82bbaa3288caf976ce1e0f5e7d15eee87
Jan Kotanski <jankotan@gmail.com>
2026-08-06 09:03:19 +0200

    fix typo

--- src/pni/nexus/xml/link_builder.cpp
+++ src/pni/nexus/xml/link_builder.cpp
@@ -35,7 +35,7 @@ LinkBuilder::LinkBuilder(const Node &node):
 
 void LinkBuilder::build(const hdf5::node::Node &parent) const
 {
-  if (!(node.attributes.exists("target")))
+  if (!(node().attributes.exists("target")))
     return;
 
   hdf5::node::Group link_parent(parent);
