b2442da42503ffbe444cce2e13d5b5243b33f272
Jan Kotanski <jankotan@gmail.com>
2026-08-07 09:47:59 +0200

    Issue 254: add support for UTF8 strings in xml builder (#279)
    * change strings to utf8
    
    * add missing hdf5:: namespace
    
    * add missing ;
    
    * add brackets
    
    * add tests
    
    * add more checks

--- test/nexus/xml/scalar_attribute_builder_test.cpp
+++ test/nexus/xml/scalar_attribute_builder_test.cpp
@@ -31,7 +31,8 @@ using namespace pni;
 
 struct ScalarAttributeFixture : public BuilderFixture
 {
-    hdf5::attribute::Attribute attribute;
+  hdf5::attribute::Attribute attribute;
+  hdf5::datatype::String string_type;
 
     ScalarAttributeFixture():
       BuilderFixture("ScalarAttributesTest.nxs","scalar_attribute_test.xml")
@@ -75,6 +76,8 @@ BOOST_AUTO_TEST_CASE(test_string_attribute)
   BOOST_CHECK(root_group.attributes.exists("string_attribute"));
   BOOST_CHECK_NO_THROW(attribute = root_group.attributes["string_attribute"]);
   BOOST_CHECK(attribute.datatype()==hdf5::datatype::create<std::string>());
+  BOOST_CHECK_NO_THROW(string_type = attribute.datatype() );
+  BOOST_CHECK(string_type.encoding()==hdf5::datatype::CharacterEncoding::UTF8);
   std::string data;
   attribute.read(data);
   BOOST_CHECK(data == "hello");
